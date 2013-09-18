2281ef51974f33669cb4581e3f83c8169c04ff50
gps13 <g.psistakis@gmail.com>
2013-09-18 22:14:17 +0300

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'e6a2768f19fa11249add1682a02160d011e4659bbf422a04cc0e0d25464f15c6ade43329931a4fbcb64cab4924c06dd739bfe9dca00c4db02a77b6da624010d0'
